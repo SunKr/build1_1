49517a8d41ec10f4433f2b0e56abd42a77203cec
VolibearSun <59292860+VolibearSun@users.noreply.github.com>
2020-09-21 19:03:00 -0400

    Add project files.

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Suncream/Desktop/new/P1_1
+# Build directory: C:/Users/Suncream/Desktop/new/build1_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "student_tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Suncream/Desktop/new/P1_1/CMakeLists.txt;17;add_test;C:/Users/Suncream/Desktop/new/P1_1/CMakeLists.txt;0;")
